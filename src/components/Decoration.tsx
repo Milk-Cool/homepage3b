5c4896aaf1f3ead8c648251a666e0ad31c289b39
Milk-Cool <coder.tomsk@gmail.com>
2026-01-31 10:07:31 +0300

    oops :D

--- src/components/Decoration.tsx
+++ src/components/Decoration.tsx
@@ -1,5 +1,5 @@
 import { CSSProperties } from "react";
 
 export default function Decoration({ src, w, h, style, className }: { src: string, w?: number, h?: number, style?: CSSProperties, className?: string }) {
-    return <img className={"decoration " + (className || "")} style={{ ...(w ? { width: `${w}vhvw` } : {}), ...(h ? { height: `${h}vh` } : {}), ...(style || {}) }} src={src} />;
+    return <img className={"decoration " + (className || "")} style={{ ...(w ? { width: `${w}vw` } : {}), ...(h ? { height: `${h}vh` } : {}), ...(style || {}) }} src={src} />;
 }
